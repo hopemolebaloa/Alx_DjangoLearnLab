75c61fec918231b0537b66dd9cafb31650f62f6f
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-02-23 18:01:59 +0000

    Fixed authentication URLs, templates, and views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ from .views import register_view
 urlpatterns = [
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", register_view, name="register"),
+    path("register/", view.register, name="register"),
 ]
 
 
