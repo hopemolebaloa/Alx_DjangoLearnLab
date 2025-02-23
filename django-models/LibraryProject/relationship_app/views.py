8b45f9543a3b89b29d56bcbdc51c1ebe6d4209b2
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-02-23 18:27:34 +0000

    Fixed @user_passes_test decorators and added missing templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,9 +2,25 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 from .models import UserProfile
 
-# Role check function for Member
+# Role check functions
+def is_admin(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == "Librarian"
+
 def is_member(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == "Member"
+
+# Admin View
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+# Librarian View
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
 
 # Member View
 @user_passes_test(is_member)
