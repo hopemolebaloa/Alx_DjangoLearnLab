b6cf1bd0b55c6c28fe07ff59257e15acbf40812c
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-02-23 18:24:51 +0000

    Fixed Member view and @user_passes_test decorator

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import admin_view, librarian_view, member_view
+from .views import admin_view, librarian_view, member_vieww
 
 urlpatterns = [
     path("admin-view/", admin_view, name="admin_view"),
     path("librarian-view/", librarian_view, name="librarian_view"),
-    path("member-view/", member_view, name="member_view"),
+     path("member-view/", member_view, name="member_view"),
 ]
 
 
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,30 +2,15 @@ from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 from .models import UserProfile
 
-# Role check functions
-def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == "Admin"
-
-def is_librarian(user):
-    return user.is_authenticated and user.userprofile.role == "Librarian"
-
+# Role check function for Member
 def is_member(user):
-    return user.is_authenticated and user.userprofile.role == "Member"
-
-# Admin View
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, "relationship_app/admin_view.html")
-
-# Librarian View
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, "relationship_app/librarian_view.html")
+    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == "Member"
 
 # Member View
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
 
 
 
