db7dcd9b14d9b4fb36d2cd604a8e2b1147137657
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-03-02 10:27:58 +0000

    yep

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -1,24 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
 
     class Meta:
         permissions = [
-            ("can_add_book", "Can add a new book"),
-            ("can_change_book", "Can edit an existing book"),
-            ("can_delete_book", "Can delete a book"),
+            ("can_view", "Can view book details"),
+            ("can_create", "Can add a new book"),
+            ("can_edit", "Can edit an existing book"),
+            ("can_delete", "Can delete a book"),
         ]
 
     def __str__(self):
         return self.title
-)
-
