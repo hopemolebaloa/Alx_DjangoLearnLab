25f43047a8920f9052e124d0f22862afa615a133
Hope Molebaloa <128815478+hopemolebaloa@users.noreply.github.com>
2025-03-02 10:29:55 +0000

    try

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -29,6 +29,19 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+class Meta:
+        permissions = [
+            ("can_view", "Can view book details"),
+            ("can_create", "Can add a new book"),
+            ("can_edit", "Can edit an existing book"),
+            ("can_delete", "Can delete a book"),
+        ]
+
     def __str__(self):
         return self.email
 
